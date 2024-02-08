9d897d3a2ceaeef5c474f9aa9098be895213fe74
Turii <o.turij@gmail.com>
2024-02-08 12:03:43 -0800

    test changed

--- tests/test_names.py
+++ tests/test_names.py
@@ -3,4 +3,4 @@ from list_models import get_list_of_models_names
 
 def test_list_models_returns_non_empty_list():
     model_names = get_list_of_models_names()
-    assert len(model_names) > 0, 'Expected at least one model in the list'
+    assert len(model_names) > 0, 'Expected at least one model in the list '
